3877b88b82df6bdc56d845c94bd0ffe9a86a6705
Alex U <auteshev505@gmail.com>
2023-06-14 10:42:53 +0100

    init config

---
+++ init.lua
@@ -0,0 +1,8 @@
+return {
+  options = {
+    g = {
+      copilot_no_tab_map = true,
+      copilot_assume_mapped = true,
+    },
+  },
+}
---
+++ mappings.lua
@@ -0,0 +1,5 @@
+return {
+  i = {
+    ["<C-a>"] = { 'copilot#Accept("<CR>")', silent = true, expr = true, replace_keycodes = false },
+  },
+}
