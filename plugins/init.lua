b67e8f05377ea5dca597bc1740da9b031c1626d8
Alex <alexu@Alexs-MBP.home>
2023-09-23 13:04:33 +0200

    add string template plugin

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,3 +1,9 @@
 return {
-  { "github/copilot.vim", lazy = false },
+	{ "github/copilot.vim", lazy = false },
+	{
+		"axelvc/template-string.nvim",
+		config = function()
+			require("template-string").setup()
+		end,
+	},
 }
