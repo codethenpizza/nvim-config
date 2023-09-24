d14c2fb08ce8ef83337e8ea23bf163d5e9c1be1a
Alex <alexu@Alexs-MBP.home>
2023-09-24 15:40:56 +0200

    update resize mappings

--- mappings.lua
+++ mappings.lua
@@ -1,15 +1,15 @@
 return {
-	i = {
-		["<C-a>"] = { 'copilot#Accept("<CR>")', silent = true, expr = true, replace_keycodes = false },
-	},
-	n = {
-		--- navigation ---------------------
-		["t<Up>"] = { "<cmd>resize +2<CR>", desc = "Resize split up" },
-		["t<Down>"] = { "<cmd>resize -2<CR>", desc = "Resize split down" },
-		["t<Left>"] = { "<cmd>vertical resize +5<CR>", desc = "Resize split left" },
-		["t<Right>"] = { "<cmd>vertical resize -5<CR>", desc = "Resize split right" },
+  i = {
+    ["<C-a>"] = { 'copilot#Accept("<CR>")', silent = true, expr = true, replace_keycodes = false },
+  },
+  n = {
+    --- navigation ---------------------
+    ["<Up>"] = { "<cmd>resize +2<CR>", desc = "Resize split up" },
+    ["<Down>"] = { "<cmd>resize -2<CR>", desc = "Resize split down" },
+    ["<Left>"] = { "<cmd>vertical resize +5<CR>", desc = "Resize split left" },
+    ["<Right>"] = { "<cmd>vertical resize -5<CR>", desc = "Resize split right" },
 
-		--- menu --------------------------
-		["<leader>td"] = { Apps.lazydocker, desc = Utils.get_icon("Docker", 1) .. "Docker" },
-	},
+    --- menu --------------------------
+    -- ["<leader>td"] = { Apps.lazydocker, desc = Utils.get_icon("Docker", 1) .. "Docker" },
+  },
 }
